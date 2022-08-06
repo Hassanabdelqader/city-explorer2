1c10d8df9a51018a55d424dc443b42191d101843
Hassanabdelqader <terawihassan@gmail.com>
2022-08-06 03:34:33 +0300

    Edit

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -13,43 +13,42 @@ export default class Footer extends React.Component {
     render() {
 
         return (
-            <MDBFooter className='bg-dark text-center text-white footer'>
+            <MDBFooter className='bg-light text-center text-white footer'>
                 <MDBContainer className='p-4 pb-0'>
                     <section className='mb-4'>
-                        <MDBBtn outline color="light  bg-light " floating className='m-1' href='#!' role='button'>
-                            <SocialIcon url="https://web.facebook.com/7asnMousa/" />
-                        </MDBBtn>
-
-                        <MDBBtn outline color="light  bg-light " floating className='m-1' href='#!' role='button'>
-                            <SocialIcon url="https://www.linkedin.com/in/hassanabdelqader/" />
-                        </MDBBtn>
-
-                        <MDBBtn outline color="light  bg-light " floating className='m-1' href='https://glittering-gumption-94e1b7.netlify.app/' role='button'>
-                            <SocialIcon url="https://glittering-gumption-94e1b7.netlify.app/" />
-                        </MDBBtn>
+                       
+                            <SocialIcon  color="light  bg-light " url="https://web.facebook.com/7asnMousa/" />
                         
-                        <MDBBtn outline color="light  bg-light " floating className='m-1' href='#!' role='button'>
-                            <a href='mailto :terawihassan@gmail.com'>
-                                <SocialIcon url="mailto:terawihassan@gmail.com" />
-                            </a>
 
-                        </MDBBtn>
+                            <SocialIcon url="https://www.linkedin.com/in/hassanabdelqader/" />
+                    
+                           <SocialIcon url="https://glittering-gumption-94e1b7.netlify.app/" />
+                      
+                        
+                           <SocialIcon url="mailto:terawihassan@gmail.com" />
+                  
 
-                        <MDBBtn outline color="light  bg-light " floating className='m-1' href='https://github.com/Hassanabdelqader' role='button'>
                             <SocialIcon url="https://github.com/Hassanabdelqader" />
-                        </MDBBtn>
+                       
 
                     </section>
                 </MDBContainer>
 
-                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                <div className='text-center text-black p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
                     © {new Date().getFullYear() } Copyright:
-                    <a className='text-white' href='https://glittering-gumption-94e1b7.netlify.app/'>
-                        Hasan Mousa
+                    <a className='text-black' href='https://glittering-gumption-94e1b7.netlify.app/'>
+                        Hasan Mousa 
                     </a>
+                                    
                 </div>
             </MDBFooter>
         );
 
     }
 }
+
+
+/*
+
+
+      */
